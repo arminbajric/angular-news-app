c0fa1007dda13cda7e839289962f1153704d7ff5
arminbajric <bajric.armin.3t1@gmail.com>
2019-02-27 20:17:47 +0100

    Angular app

--- src/app/category-view/category-view.component.ts
+++ src/app/category-view/category-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import { NewsServiceService } from '../services/news-service.service';
 @Component({
   selector: 'app-category-view',
   templateUrl: './category-view.component.html',
@@ -10,12 +11,18 @@ import {environment} from '../../environments/environment';
 export class CategoryViewComponent implements OnInit {
 @Input() news:any;
 @Input() dataLoadingDone:boolean;
-  constructor(private router:Router,private http:HttpClient) { 
+pages:any;
+currentUrl:any;
+  constructor(private router:Router,private http:HttpClient,private route:ActivatedRoute,private newsService:NewsServiceService) { 
     this.dataLoadingDone=false;
+    this.pages=['1','2','3','4','5'];
+   
+    
   }
    
   ngOnInit() {
-
+    this.currentUrl=this.route.url;
+    console.log(this.currentUrl);
     this.http.get(environment.baseApi+this.router.url,{
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json'),
@@ -36,4 +43,16 @@ export class CategoryViewComponent implements OnInit {
   
  
   }
+  updatePages(page){
+    this.dataLoadingDone=false;
+    let category:String=this.news[0].category;
+    category=category.charAt(0).toLowerCase()+category.substring(1);
+    
+    const route='/home/'+category+'/pages'
+    this.newsService.getCategoryPage(page,route).subscribe(response=>{
+      this.news=response.body;
+      this.dataLoadingDone=true;
+    });
+  }
+  
 }
--- src/app/services/news-service.service.ts
+++ src/app/services/news-service.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsServiceService {
 
   constructor(private _router: Router, private http: HttpClient) { }
-  getCategoryNews(category){
-    const news={};
-    return news;
+  getCategoryPage(page, route): Observable<any> {
+    return this.http.get(environment.baseApi + route, {
+      headers: new HttpHeaders().set('Content-type', 'aplication/json'),
+      params: new HttpParams().set('page', page),
+      observe:'response'
+    }).pipe(map(response=>{return response}));
+
   }
 }
